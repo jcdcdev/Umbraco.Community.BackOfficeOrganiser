11e1889e703da115eac379281e996f7dd3725460
jcdcdev <james@jcdc.dev>
2025-04-22 01:27:21 +0100

    fix: PackageManifest version

--- src/Umbraco.Community.BackOfficeOrganiser/Composing/PackageManifestReader.cs
+++ src/Umbraco.Community.BackOfficeOrganiser/Composing/PackageManifestReader.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using jcdcdev.Umbraco.Core.Extensions;
 using jcdcdev.Umbraco.Core.Web.Models.Manifests;
 using Umbraco.Cms.Core.Manifest;
@@ -5,15 +6,15 @@ using Umbraco.Cms.Infrastructure.Manifest;
 
 namespace Umbraco.Community.BackOfficeOrganiser.Composing;
 
-public class BackOfficeOrganiserPackageManifestReader : IPackageManifestReader
+public class PackageManifestReader : IPackageManifestReader
 {
-    public async Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
+    public Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
     {
         var extensions = new List<IManifest>();
         var packageManifest = new PackageManifest
         {
             Name = Constants.PackageName,
-            Version = EnvironmentExtensions.CurrentAssemblyVersion().ToSemVer()?.ToString() ?? "0.1.0",
+            Version = Assembly.GetAssembly(typeof(PackageManifestReader))?.GetName().Version?.ToSemVer()?.ToString() ?? "0.1.0",
             AllowPublicAccess = false,
             AllowTelemetry = false,
             Extensions = []
@@ -27,6 +28,6 @@ public class BackOfficeOrganiserPackageManifestReader : IPackageManifestReader
         });
 
         packageManifest.Extensions = extensions.OfType<object>().ToArray();
-        return [packageManifest];
+        return Task.FromResult<IEnumerable<PackageManifest>>([packageManifest]);
     }
 }
